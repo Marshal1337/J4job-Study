be955ed01f99a2f5ee631a854fde01a5ff48e50e
Marshal1337 <chevgeny@mail.ru>
2016-11-21 22:05:05 +0300

    Changed lesson_3

--- Module_1/Lesson_3/src/main/java/сhemagin/Max.java
+++ Module_1/Lesson_3/src/main/java/сhemagin/Max.java
@@ -1,7 +1,7 @@
 package chemagin;
 
 /**
- *Class Класс, вычисляющий максимальное из двух чисел.
+ *Class  ласс, вычисл¤ющий максимальное из двух чисел.
  *@author Chemagin Evgeny
  *@version 1
  *@since 17.11.2016
@@ -10,26 +10,26 @@ package chemagin;
  public class Max {
 
 /**
- *Метод, возвращающий максимальное из двух чисел.
+ *ћетод, возвращающий максимальное из двух чисел.
  *@param first первое число.
  *@param second второе число.
- *@return result результат.
+ *@return результат.
  */
 
 	public int max(int first, int second) {
-		int result = first > second ? first : second;
-		return result;
+		return first > second ? first : second;
 	}
 
 /**
- *Метод, возвращающий максимальное из трёх чисел.
+ *ћетод, возвращающий максимальное из трЄх чисел.
  *@param first первое число.
  *@param second второе число.
  *@param third третье число.
- *@return result результат.
+ *@return результат.
  */
+
 	public int triplemax(int first, int second, int third) {
-		int result = this.max(first, second) > third ? this.max(first, second) : third;
-		return result;
+		first = this.max(first, second);
+		return this.max(first, third);
 	}
 }
